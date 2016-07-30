d0a835f7cccc3802175c684a17cae47102a190e9
C0chett0 <Cochetto75@gmail.com>
2016-07-30 18:08:55 +0200

    Hello World !

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 
 module.exports.loop = function () {
+    var Spawn1 = 'Mézon';
 
     for(var name in Memory.creeps) {
         if(!Game.creeps[name]) {
@@ -12,14 +13,20 @@ module.exports.loop = function () {
 
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
     var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+    var repairers = _.filter(Game.creeps, (creep) => creep.memory.role == 'repair');
     //console.log('Harvesters: ' + harvesters.length);
 
-    if(harvesters.length < 10 && Game.spawns['Spawn1'].energy >= 300) {
-        var newName = Game.spawns['Spawn1'].createCreep([WORK,WORK,CARRY,MOVE], undefined, {role: 'harvester'});
+    /*if(repairers.length < 1 && Game.spawns[Spawn1].energy >= 300) {
+        var newName = Game.spawns[Spawn1].createCreep([WORK,CARRY,MOVE], undefined, {role: 'repair'});
+        console.log('Spawning new harvester: ' + newName);
+    }*/
+
+    if(harvesters.length < 10 && Game.spawns[Spawn1].energy >= 300) {
+        var newName = Game.spawns[Spawn1].createCreep([WORK,WORK,CARRY,MOVE], undefined, {role: 'harvester'});
         console.log('Spawning new harvester: ' + newName);
     }
-    if(upgraders.length < 50 && Game.spawns['Spawn1'].energy >= 300) {
-        var newName = Game.spawns['Spawn1'].createCreep([WORK,WORK,CARRY,MOVE], undefined, {role: 'upgrader'});
+    if(upgraders.length < 50 && Game.spawns[Spawn1].energy >= 300) {
+        var newName = Game.spawns[Spawn1].createCreep([WORK,WORK,CARRY,MOVE], undefined, {role: 'upgrader'});
         console.log('Spawning new upgrader: ' + newName);
     }
 
