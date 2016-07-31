6566f3358a687010a363165e0bb568329f645885
C0chett0 <Cochetto75@gmail.com>
2016-07-31 03:48:38 +0200

    Hello World !

--- role.harvester.js
+++ role.harvester.js
@@ -3,27 +3,9 @@ var roleHarvester = {
     /** @param {Creep} creep **/
     run: function(creep) {
         if(creep.carry.energy < creep.carryCapacity) {
-            targets = creep.pos.findInRange(FIND_SOURCES, 1)
-            if (targets.length > 0) {
-                creep.harvest(targets[0]);
-                creep.memory.sourceDestination = undefined;
-            } else {
-                if (creep.memory.sourceDestination != undefined) {
-                    creep.moveTo(Game.getObjectById(creep.memory.sourceDestination));
-                } else {
-                    var sources = creep.room.find(FIND_SOURCES);
-                    if (sources.length > 0) {
-                        var seed = _.floor(_.random() * (3)) + 1;
-                        if (seed > 1) {
-                            idTab = 0
-                        } else {
-                            idTab = 1
-                        }
-
-                        creep.memory.sourceDestination = sources[idTab].id;
-                        creep.moveTo(sources[seed]);
-                    }
-                }
+            var target = creep.pos.findClosestByPath(FIND_DROPPED_ENERGY);
+            if (creep.pickup(target) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(target);
             }
         }
         else {
