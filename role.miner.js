c630be4bbdff84a49ac8121ec87912b730bd6afb
C0chett0 <Cochetto75@gmail.com>
2016-07-31 03:39:30 +0200

    Hello World !

--- role.miner.js
+++ role.miner.js
@@ -3,13 +3,18 @@ var roleMiner = {
     /** @param {Creep} creep **/
     run: function(creep, map) {
         if(creep.memory.mySpot == undefined) {
-            map.miningSpots.forEach((spot) => {
+            var flag = false;
+            var i = 0;
+            while(!flag && i < map.miningSpots.length) {
+                var spot = map.miningSpots[i];
                 if (!spot.reserved || Game.creeps[spot.reserved] == undefined)
                 {
                     creep.memory.mySpot = [spot.x, spot.y];
                     spot.reserved = creep.name;
+                    flag = true;
                 }
-            });
+                i++;
+            }
         }
         if (creep.pos.x != creep.memory.mySpot[0] || creep.pos.y != creep.memory.mySpot[1]) {
             creep.moveTo(creep.memory.mySpot[0], creep.memory.mySpot[1]);
