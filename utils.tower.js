553fd7e37601e4d3d2179de3c43b779414930e61
Laurent Cochet <laurent.cochet@elbee.fr>
2016-08-01 16:02:35 +0200

    Test

---
+++ utils.tower.js
@@ -0,0 +1,20 @@
+var towerUitls = {
+    run: function (tower) {
+        var closestBadGuy = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if (closestBadGuy != undefined) {
+            tower.attack(closestBadGuy);
+        } else {
+            var closestGoodGuy = tower.pos.findClosestByRange(FIND_MY_CREEPS, {filter: (c) => c.hits < c.hitsMax});
+            if (closestGoodGuy != undefined) {
+                tower.heal(closestGoodGuy);
+            } else {
+                var closestDamagedStructure = tower.pos.findClosestByRange(FIND_MY_STRUCTURES, {filter: (s) => s.hits < s.hitsMax});
+                if (closestDamagedStructure != undefined) {
+                    tower.repair(closestDamagedStructure);
+                }
+            }
+        }
+    }
+};
+
+module.exports = towerUitls;
